4bdc353bf559d2fc361b2e70aa64e57dfff4eb6c
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-09-19 12:22:14 +0200

    calculation of snailtower

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,29 @@
 package com.appslab;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        Scanner consoleIn = new Scanner(System.in);
+
+        System.out.println("Write height of step: ");
+        double heightOfStep = consoleIn.nextDouble();
+
+        System.out.println("Write length of one step: ");
+        double lengthOfStep = consoleIn.nextDouble();
+
+        System.out.println("Write height of the tower: ");
+        double heightOfTower = consoleIn.nextDouble();
+
+        if(heightOfTower > 0 && heightOfStep > 0 && lengthOfStep > 0){
+
+            double totalDistance = ((heightOfTower/heightOfStep)*(heightOfStep+lengthOfStep));
+            System.out.println("Total Distance is: "+totalDistance);
+        }
+        else{
+            System.out.println("Your values were lower than 0, try again");
+        }
     }
 }
