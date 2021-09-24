b31eccbddc39aec42100cda0c76335316e0257b6
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-09-24 17:34:22 +0200

    make an function that simulates a rock paper scissors game

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -7,30 +7,40 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner consoleIn = new Scanner(System.in);
+        System.out.println("Write rock, paper or scissors");
 
-        System.out.println("Write Number Of Slices: ");
-        int numberOfSlices = consoleIn.nextInt();
+        System.out.println("PLAYER 1: ");
+        String player1 = consoleIn.nextLine();
 
-        System.out.println("Write Number Of Recipients: ");
-        int numberOfRecipients = consoleIn.nextInt();
+        System.out.println("PLAYER 2: ");
+        String player2 = consoleIn.nextLine();
 
-        System.out.println("Write How Many Slices Each Person Gets: ");
-        int slicesEach = consoleIn.nextInt();
-
-        System.out.println(EqualPieSlices(numberOfSlices, numberOfRecipients, slicesEach));
+        System.out.println(WhoIsTheWinner(player1, player2));
     }
 
-    public static boolean EqualPieSlices(int numberOfSlices, int numberOfRecipients, int slicesEach){
+    public static String WhoIsTheWinner(String player1, String player2){
 
-        boolean equal = true;
+        String result = "";
 
-        if((numberOfRecipients*slicesEach) > numberOfSlices){
-            equal = false;
+        if(player1.equals("rock") && player2.equals("rock") || player1.equals("scissors") && player2.equals("scissors")){
+            result = "TIE";
         }
-        else if ((numberOfRecipients*slicesEach) < numberOfSlices){
-            equal = true;
+        else if(player1.equals("paper") && player2.equals("paper")){
+            result = "TIE";
+        }
+        else if (player1.equals("scissors") && player2.equals("paper")){
+            result = "PLAYER 1 WINS!";
+        }
+        else if(player1.equals("paper") && player2.equals("rock")){
+            result = "PLAYER 1 WINS!";
+        }
+        else if(player1.equals("rock") && player2.equals("scissors")){
+            result = "PLAYER 1 WINS!";
+        }
+        else{
+            result = "PLAYER 2 WINS!";
         }
 
-        return equal;
+        return result;
     }
 }
