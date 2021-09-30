420708d8cf029f8e93545e108e9c900187439659
JaroslavOndrus <jaroslavondrus04@gmail.com>
2021-09-30 18:58:12 +0200

    create a function that writes the biggest and the smallest number of array and prints their difference

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -7,22 +7,28 @@ public class Main {
     public static void main(String[] args) {
 
         //Scanner consoleIn = new Scanner(System.in);
+        int array1[] = {44, 32, 86, 19};
+        int biggestNumber;
+        int smallestNumber;
 
-        String[] rok1 = {"A8","E8"};
-        String[] rok2= {"A1","B2"};
-        String[] rok3 = {"H4","H3"};
-        String[] rok4 = {"F5","C8"};
+        biggestNumber = smallestNumber = array1[0];
 
-        System.out.print(TwoRooksAttack(rok1) +"\n");
-        System.out.print(TwoRooksAttack(rok2)+"\n");
-        System.out.print(TwoRooksAttack(rok3)+"\n");
-        System.out.print(TwoRooksAttack(rok4));
+        for(int i = 1; i < array1.length; i++) {
 
-    }
-    public static boolean TwoRooksAttack(String[] rok)
-    {
-        String rok1 = rok[0];
-        String rok2 = rok[1];
-        return rok1.charAt(0) == rok2.charAt(0) || rok1.charAt(1) == rok2.charAt(1);
+            if(biggestNumber < array1[i]){
+                biggestNumber = array1[i];
+            }
+        }
+
+        for(int i = 0; i < array1.length; i++){
+
+            if(smallestNumber > array1[i]){
+                smallestNumber = array1[i];
+            }
+        }
+
+        System.out.println("The Smallest Number is: " + smallestNumber);
+        System.out.println("The Biggest Number is: " + biggestNumber);
+        System.out.println("The Difference Between The Biggest Number And The Smallest One Is: " + (biggestNumber - smallestNumber));
     }
 }
